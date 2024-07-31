fdd2c9db3e52afe1d1a9dbbd89d22b0cb2841636
zahran <zahranraifalhadil1107@gmail.com>
2024-07-31 11:22:39 +0700

    auth

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\Api\LigaController;
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\FanController;
+use App\Http\Controllers\Api\KlubController;
 use App\Http\Controllers\Api\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -9,6 +11,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('logout',[AuthController::class, 'logout']);
+
 
 // Route::get('liga', [LigaController::class, 'index']);
 // Route::post('liga', [LigaController::class, 'store']);
@@ -19,3 +25,8 @@ Route::resource('liga', LigaController::class)->except(['edit', 'create']);
 Route::resource('klub', KlubController::class)->except(['edit', 'create']);
 Route::resource('pemain', PemainController::class)->except(['edit', 'create']);
 Route::resource('fan', FanController::class)->except(['edit', 'create']);
+});
+//auth route
+
+Route::post('register',[AuthController::class, 'register']);
+Route::post('login',[AuthController::class, 'login']);
